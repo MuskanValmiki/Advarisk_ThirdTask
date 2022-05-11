066a5af4603b99b1b9d54d35a0b5974bf5e73baa
MuskanValmiki <muskanvalmikee@gmail.com>
2022-05-11 13:41:18 +0530

    here we scrap colony data and store in saperate table.

--- thirdtask/spiders/colony_data.py
+++ thirdtask/spiders/colony_data.py
@@ -12,24 +12,21 @@ class colony_data(scrapy.Spider):
             callback=self.parse) 
    
     def parse(self,response):
-        data=response.xpath('//select/option/text()')[5:].getall()
-        colony_list=[]
-        for colony in data:
-            colony_list.append(colony)
-            yield Request('http://nwcmc.gov.in/ptsearch_data.php?colony={colony}&houseno=&name=&address=&serno=',
-                meta={'cookiejar':str(uuid4)},
+        data=response.xpath('//*[@id="colony"]/option/@value')[1:11].getall()
+        for colony_no in data:
+            print(colony_no,'***************************************************************')
+            yield Request('http://nwcmc.gov.in/ptsearch_data.php?colony={}&houseno=&name=&address=&serno='.format(colony_no),
+                meta={'cookiejar':str(uuid4),'colony':colony_no},
                 callback=self.get_colonies)
 
-            def get_colonies(self,response):
-                table=response.xpath('/html/body/table')
-                with open('colony_data.csv','w') as f:
-                    wr=csv.writer(f)
-                    for tr in table.xpath('.//tr'):
-                        _tdata=[]
-                    
-                        for td in tr.xpath('.//td'):
-                            _text=''.join([a for a in td.xpath('.//text()').extract()])
-                            _tdata.append(_text)
-                        wr.writerow(_tdata)
-    
-    
+    def get_colonies(self,response):
+        table=response.xpath('/html/body/table')
+        with open('colony_data_{}.csv'.format(response.meta['colony']),'a') as f:
+            wr=csv.writer(f)
+            for tr in table.xpath('.//tr'):
+                _tdata=[]
+                        
+                for td in tr.xpath('.//td'):
+                    _text=''.join([a for a in td.xpath('.//text()').extract()])
+                    _tdata.append(_text)
+                wr.writerow(_tdata)
